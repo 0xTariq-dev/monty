62fe3c6d79b58bd90befae589ac58ff0603bd122
Hesham <dev@heshamorabi.com>
2023-09-21 16:08:03 -0700

    pint function

--- monty.h
+++ monty.h
@@ -67,6 +67,7 @@ int exec_op(stack_t **stack, char *argument, char *element, int n);
 void push(stack_t **stack, unsigned int n);
 void pall(stack_t **stack, unsigned int n);
 void free_stack(stack_t *stack);
+void pint(stack_t **stack, unsigned int line_number);
 
 /*Error Handling*/
 void err_push(FILE *mon, char *line, stack_t *stack, int count);
