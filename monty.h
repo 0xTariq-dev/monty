b9988a0a5fd24f867e5bd15f7ba4958d313bec59
Hesham <dev@heshamorabi.com>
2023-09-21 17:13:12 -0700

    final

--- monty.h
+++ monty.h
@@ -69,6 +69,8 @@ void pall(stack_t **stack, unsigned int n);
 void pop(stack_t **stack, unsigned int n);
 void free_stack(stack_t *stack);
 void pint(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int n);
+void nop(stack_t **stack, unsigned int n);
 
 /*Error Handling*/
 void err_push(FILE *mon, char *line, stack_t *stack, int count);
