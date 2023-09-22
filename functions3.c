c1acc4f190594a36a4b909559b087144f9e3768f
Hesham <dev@heshamorabi.com>
2023-09-21 17:12:27 -0700

    nop

---
+++ functions3.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - useless func
+ * @stack: the stack.
+ * @n: the number of the line.
+ */
+void nop(stack_t **stack, unsigned int n)
+{
+	(void)stack;
+	(void)line_number;
+}
